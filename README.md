4b8b7e4802d587b115f07e47891f63e561847bc5
Anaëlle <a.pierredon@lend-rxtech.com>
2026-04-20 17:01:18 +0200

    change of dataset

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # spam_classification
 Classification model for spam detection from a Kaggle dataset.
 
-Dataset from Kaggle :  ([link](https://www.kaggle.com/datasets/ssssws/spam-email-detection-dataset-clean-and-ml-ready)) 
+Dataset from Kaggle :  ([link](https://www.kaggle.com/datasets/willyard/spam-email-dataset)) 
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,16 +2,18 @@ import pandas as pd
 import re
 from sklearn.model_selection import train_test_split
 
-DATASET_PATH = '../data/spam_email_dataset.csv'
+DATASET_PATH = '../data/enron_spam_data.csv'
+TEXT_COLUMNS = ["Subject", "Message", "Message_full"]
 
 def get_data(path=DATASET_PATH):
     df = pd.read_csv(path)
-    df.set_index("email_id", inplace=True)
-    df["subject"] = df["subject"].fillna("")
-    df["email_text"] = df["email_text"].fillna("")
-    df["label_name"] = df["label"].apply(lambda x: "SPAM" if x == 1 else "HAM")
-    df["email_full"] = df["subject"] + " - " + df["email_text"]
-    df = df[["subject", "email_text", "email_full", "label", "label_name"]]
+    df.set_index("Message ID", inplace=True)
+    df["Subject"] = df["Subject"].fillna("")
+    df["Message"] = df["Message"].fillna("")
+    df["Message_full"] = df["Subject"] + " - " + df["Message"]
+    df["Label"] = df["Spam/Ham"]
+    df.drop_duplicates(subset=["Message_full"], inplace=True)
+    df = df[["Subject", "Message", "Message_full", "Label"]]
     return df
 
 def clean(text):
@@ -21,18 +23,18 @@ def clean(text):
 
 def data_cleaning(df, text_columns=None):
     if text_columns is None:
-        text_columns = ["subject", "email_text", "email_full"]
+        text_columns = TEXT_COLUMNS
     df = df.copy()
     for col in text_columns:
         df[col] = df[col].apply(lambda x: clean(x))
     return df
 
 def split_data(df, test_size=0.2):
-    X_train, X_test, y_train, y_test = train_test_split(df["email_full"],
-                                                        df["label"],
+    X_train, X_test, y_train, y_test = train_test_split(df["Message_full"],
+                                                        df["Label"],
                                                         test_size=test_size,
                                                         random_state=42,
-                                                        stratify=df["label"])
+                                                        stratify=df["Label"])
     return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
