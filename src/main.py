b89d92543175049a3770e4bb153b93b269d81f25
Anaëlle <a.pierredon@lend-rxtech.com>
2026-04-20 18:57:49 +0200

    save model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from data_preprocessing import get_data, data_cleaning, split_data
 from modeling import train_model, evaluate_model
+import joblib
 
 # Get dataset and clean it
 df = data_cleaning(get_data())
@@ -7,5 +8,6 @@ df = data_cleaning(get_data())
 X_train, X_test, y_train, y_test = split_data(df)
 # Vectorizer and training
 model = train_model(X_train, y_train)
+joblib.dump(model, "../models/spam_classifier.pkl")
 # Evaluation
 evaluate_model(model, X_test, y_test)
